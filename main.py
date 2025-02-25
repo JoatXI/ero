e337669ca8756b6d0982d37d9c15dcdbfcb5f704
Joat <michaelpaterson194@gmail.com>
2025-02-25 23:50:46 +0000

    Saving Screen Capture:
    Moved the MSS screen capturing into a defined function.
    Set up a consistent Frames Per Second (FPS) maintainance.
    Created an array list to hold captured frames.
    Defined a function to convert captured frames into video using cv2.
    Set up capture thread to ensure smooth video recording.
    NEXT UPDATE: Audio Recording && Threading of video and audio recording process.

--- main.py
+++ main.py
@@ -1,26 +1,49 @@
 from mss import mss
+from ffmpeg import FFmpeg
 import numpy as np
 import time
 import cv2
+import threading
 
-with mss() as sct:
-    # Define screen capture settings
-    monitor = {"top": 0, "left": 0, "width": 1920, "height": 1080}
+running = True
+frames = []
+FPS = 30
+
+def screen_capture():
+    with mss() as sct:
+        # Define screen capture settings
+        monitor = {"top": 0, "left": 0, "width": 1920, "height": 1080}
+        
+        while running:
+            last_time = time.time()
+            
+            # Capture screen frame
+            img = np.array(sct.grab(monitor))
+            
+            # cv2.imshow("OpenCV/Numpy normal", img)
+            
+            frames.append(img)
+            time_diff = time.time() - last_time
+            time.sleep(max(1.0 / FPS - time_diff, 0))
+        
+def save_recording():
+    height, width, _ = frames[0].shape
+    video_writer = cv2.VideoWriter('default_video.mp4', cv2.VideoWriter_fourcc(*'mp4v'), FPS, (width, height))
     
-    for _ in range(120):
-        last_time = time.time()
-        
-        # Capture screen frame
-        img = np.array(sct.grab(monitor))
-        
-        cv2.imshow("OpenCV/Numpy normal", img)
-        
-        # Display the picture in grayscale
-        # cv2.imshow('OpenCV/Numpy grayscale', cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY))
-
-        print(f"fps: {1 / (time.time() - last_time)}")
-
-        # Press "q" to quit
-        if cv2.waitKey(25) & 0xFF == ord("q"):
-            cv2.destroyAllWindows()
-            break
+    for img in frames:
+        video_writer.write(cv2.cvtColor(img, cv2.COLOR_RGBA2RGB))
+    video_writer.release()
+    
+    
+def main():
+    global running
+    threading.Thread(target=screen_capture).start()
+    
+    time.sleep(10) # Sets recording duration
+    running = False
+    
+    print('Saving video...')
+    save_recording()
+    print('Video saved.')
+    
+main()
