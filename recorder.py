9de24ce583e305493f370e0e647827269f8eb17d
Joat <michaelpaterson194@gmail.com>
2025-03-15 17:46:30 +0000

    MSS Stream Modification && FFmpeg Pipping:
    Moved the mss screen capturing function into a separate program/file.
    Utilised the python sys module to write/output the captured frame with the stdout.buffer wrapper.
    Used python's subprocess to pipe in the frame output from mss in the FFmpeg encoding function.
    This enables the Graceful termination of the running FFmpeg process as FFmpeg was started using the .execute FFmpeg function.

--- recorder.py
+++ recorder.py
@@ -1,7 +1,5 @@
 from ffmpeg import FFmpeg, Progress, FFmpegError
 from win32api import GetSystemMetrics
-from mss import mss, exception
-import numpy as np
 import subprocess
 import threading
 import keyboard
@@ -11,69 +9,55 @@ import time
 WIDTH, HEIGHT = GetSystemMetrics(0), GetSystemMetrics(1) # Gets monitor dimensions
 running = True
 FPS = 30
-OUTPUT_FILE = f"{datetime.datetime.now().strftime("%Y_%m_%d %H_%M_%S")}.mkv"
+OUTPUT_FILE = f"{datetime.datetime.now().strftime('%Y_%m_%d %H_%M_%S')}.mkv"
 
 def ffmpeg_encoder():
     # Can we tell ffmpeg to read in from memory (i.e. your frames) as an input source, rather than standard input?
     AUDIO_DEVICE = "Stereo Mix (Realtek(R) Audio)"
     
     try:
+        mss_stream = subprocess.Popen(
+            'python screen_capture.py',
+            stdout=subprocess.PIPE
+        )
+        
         ffmpeg = (
             FFmpeg()
             .option("y")  # Overwrite existing file
-            .input(f"audio={AUDIO_DEVICE}", f="dshow", ac=2, ar=44100, audio_buffer_size="80m", t=10)
+            .input(f"audio={AUDIO_DEVICE}", f="dshow", ac=2, ar=44100, audio_buffer_size="80m")
             .input("pipe:0", f="rawvideo", s=f"{WIDTH}x{HEIGHT}", pix_fmt="bgra", r=FPS)
             .output(OUTPUT_FILE, vcodec="libx264", pix_fmt="yuv420p")
         )
         
+        @ffmpeg.on("progress")
+        def time_to_terminate(progress: Progress):
+            # If you have recorded more than 200 frames, stop recording
+            print(progress)
+            if not running:
+                print('\nterminating recording...')
+                ffmpeg.terminate()
+                
         # Start FFmpeg process using stdin for real-time frame input
-        return subprocess.Popen(ffmpeg.arguments, stdin=subprocess.PIPE)
+        print('\nencoding video...\n')
+        ffmpeg.execute(mss_stream.stdout)
     
     except FFmpegError as exception:
         print("An exception has occurred!")
         print("- Message from ffmpeg:", exception.message)
         print("- Arguments to execute ffmpeg:", exception.arguments)
 
-def screen_capture():
-    try:
-        with mss() as sct:
-            # Define screen monitor capture settings
-            monitor = {"top": 0, "left": 0, "width": WIDTH, "height": HEIGHT}
-            
-            process = ffmpeg_encoder()
-            while running:
-                # Capture screen frame
-                frame = np.array(sct.grab(monitor))
-                process.stdin.write(frame.tobytes()) # write raw bytes to standard input?
-                
-                time.sleep(1.0 / FPS)
-            
-            process.stdin.close()
-            process.wait()
-            
-    except exception.ScreenShotError as e:
-        print("An error occurred while acquiring screen capture: ", e)
-
-def end_recording():
+def main():
     global running
-    print("\n",keyboard.read_key())
+    
+    capture_thread = threading.Thread(target=ffmpeg_encoder)
+    capture_thread.start()
     
     if keyboard.read_key() == "esc":
         print("\nStopping Recording...")
         running = False
+        capture_thread.join()
+        
+        print('\nVideo saved.')
 
-def main():
-    global running
-    
-    stopper = threading.Thread(target=end_recording, daemon=True)
-    stopper.start()
-    
-    capture_thread = threading.Thread(target=screen_capture, daemon=True)
-    capture_thread.start()
-    
-    capture_thread.join()
-    
-    print('Video saved.')
-    
 if __name__ == '__main__':
     main()
---
+++ screen_capture.py
@@ -0,0 +1,44 @@
+from win32api import GetSystemMetrics
+from mss import mss, exception
+import numpy as np
+import threading
+import keyboard
+import time
+import sys
+
+WIDTH, HEIGHT = GetSystemMetrics(0), GetSystemMetrics(1) # Gets monitor dimensions
+running = True
+FPS = 30
+
+def screen_capture():
+    try:
+        with mss() as sct:
+            # Define screen monitor capture settings
+            monitor = {"top": 0, "left": 0, "width": WIDTH, "height": HEIGHT}
+            
+            while running:
+                last_time = time.time()
+                
+                frame = np.array(sct.grab(monitor))
+                sys.stdout.buffer.write(frame.tobytes())
+                
+                time_diff = time.time() - last_time
+                time.sleep(max(1.0 / FPS - time_diff, 0))
+                
+            sys.stdout.buffer.close()
+            
+    except exception.ScreenShotError as e:
+        print("An error occurred while acquiring screen capture: ", e)
+
+def main():
+    global running
+    
+    capture_thread = threading.Thread(target=screen_capture)
+    capture_thread.start()
+    
+    if keyboard.read_key() == "esc":
+        running = False
+        capture_thread.join()
+
+if __name__ == '__main__':
+    main()
