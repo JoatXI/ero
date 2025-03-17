863fbd7e23a525a902194b5e2e914b274b19914f
Joat <michaelpaterson194@gmail.com>
2025-03-17 17:16:31 +0000

    Created classes for recorder.py and screen_capture.py:
    Created two classes in the recorder.py program.
    Setup an Encoder class with a method that helps detect users system hardware for setting ffmpeg encoder.
    Setup a ScreenRecorder class with a constructor that takes output file name, screen dimension, selected encoder etc..
    Created ffmpeg_encoder as a method in the ScreenRecorder class that encodes the video && audio from MSS and dshow respectively.
    Setup a start_recording method that is responsible for starting the recording by setting up a thread for the ffmpeg_encoder method.
    Setup a FrameCapture class in the screen_capture program with a screen_capture method that captures users screen and sends each captured frame to the recorder.py program using the sys.stdout.buffer module.
    Created a requirements.txt file containing the necessary libraries to run the program.
    Added an audio codec  for the dshow audio input.

--- recorder.py
+++ recorder.py
@@ -1,60 +1,100 @@
 from ffmpeg import FFmpeg, Progress, FFmpegError
-from win32api import GetSystemMetrics
 import subprocess
 import threading
+import pyautogui
 import keyboard
 import datetime
-import time
+import platform
+import cpuinfo
+import GPUtil
 
-WIDTH, HEIGHT = GetSystemMetrics(0), GetSystemMetrics(1) # Gets monitor dimensions
-running = True
-FPS = 30
-OUTPUT_FILE = f"{datetime.datetime.now().strftime('%Y_%m_%d %H_%M_%S')}.mkv"
+class Encoder:
+    def __init__(self):
+        self.nvidia_gpu = GPUtil.getGPUs()
+        self.info = cpuinfo.get_cpu_info()
+        self.os = platform.system()
+        self.amd_gpu = False
+    
+    def set_encoder(self):
+        print(self.os)
+        
+        if self.os == "Windows":
+            process = subprocess.check_output(["wmic", "path", "win32_VideoController", "get", "name"]).decode()
+            self.amd_gpu = any(keyword in process for keyword in ["AMD", "Radeon"])
+        elif self.os == "Linux":
+            output = subprocess.check_output("lspci", shell=True).decode()
+            self.amd_gpu = any("AMD" in line or "Radeon" in line for line in output.splitlines())
 
-def ffmpeg_encoder():
-    AUDIO_DEVICE = "Stereo Mix (Realtek(R) Audio)"
-    
-    try:
-        mss_stream = subprocess.Popen(
-            'python screen_capture.py',
-            stdout=subprocess.PIPE
-        )
-        
-        ffmpeg = (
-            FFmpeg()
-            .option("y")
-            .input("pipe:0", f="rawvideo", s=f"{WIDTH}x{HEIGHT}", pix_fmt="bgra", r=FPS)
-            .input(f"audio={AUDIO_DEVICE}", rtbufsize="1024M", f="dshow", ac=2, ar=44100, channel_layout="stereo", audio_buffer_size="80m")
-            .output(OUTPUT_FILE, vcodec="libx264", pix_fmt="yuv420p", preset="slow", crf=22, shortest=None)
-        )
-        
-        @ffmpeg.on("progress")
-        def time_to_terminate(progress: Progress):
-            print(progress)
-            if not running:
-                print('\nterminating recording...')
-                ffmpeg.terminate()
-                
-        print('\nencoding video...\n')
-        ffmpeg.execute(mss_stream.stdout)
-    
-    except FFmpegError as exception:
-        print("An exception has occurred!")
-        print("- Message from ffmpeg:", exception.message)
-        print("- Arguments to execute ffmpeg:", exception.arguments)
+        try:
+            if "Intel" in self.info.get("brand_raw", "") and self.nvidia_gpu:
+                return "h264_nvenc"
+            elif "Intel" in self.info.get("brand_raw", "") and not self.nvidia_gpu:
+                return "h264_qsv"
+            elif "AMD" in self.info.get("brand_raw", "") and self.nvidia_gpu:
+                return "h264_nvenc"
+            elif "AMD" in self.info.get("brand_raw", "") or self.amd_gpu:
+                return "h264_amf"
+            else:
+                return "libx264"
+        except Exception as e:
+            print("Error setting encoder:", e)
+            return "libx264"
 
-def main():
-    global running
-    
-    capture_thread = threading.Thread(target=ffmpeg_encoder)
-    capture_thread.start()
-    
-    if keyboard.read_key() == "esc":
-        print("\nStopping Recording...")
-        running = False
-        capture_thread.join()
+class ScreenRecorder:
+    def __init__(self, encoder, fps=30, audio_device="Stereo Mix (Realtek(R) Audio)"):
+        self.file_name = f"{datetime.datetime.now().strftime('%Y_%m_%d %H_%M_%S')}.mkv"
+        self.width, self.height = pyautogui.size()
+        self.audio_device = audio_device
+        self.encoder = encoder
+        self.running = False
+        self.fps = fps
         
-        print('\nVideo saved.')
+    def ffmpeg_encoder(self):
+        try:
+            mss_stream = subprocess.Popen(
+                'python screen_capture.py',
+                stdout=subprocess.PIPE
+            )
+            
+            ffmpeg = (
+                FFmpeg()
+                .option("y")
+                .input("pipe:0", f="rawvideo", s=f"{self.width}x{self.height}", pix_fmt="bgra", r=self.fps)
+                .input(f"audio={self.audio_device}", rtbufsize="1024M", f="dshow", ac=2, ar=44100, channel_layout="stereo", audio_buffer_size="80m")
+                .output(self.file_name, acodec="copy", vcodec=self.encoder, pix_fmt="yuv420p", preset="slow", crf=22, shortest=None)
+            )
+            
+            @ffmpeg.on("progress")
+            def time_to_terminate(progress: Progress):
+                print(progress)
+                if not self.running:
+                    print('\nterminating recording...')
+                    ffmpeg.terminate()
+                    
+            print('\nencoding video...\n')
+            ffmpeg.execute(mss_stream.stdout)
+        
+        except FFmpegError as exception:
+            print("An exception has occurred!")
+            print("- Message from ffmpeg:", exception.message)
+            print("- Arguments to execute ffmpeg:", exception.arguments)
+
+    def start_recording(self):
+        self.running = True
+        
+        encoding_thread = threading.Thread(target=self.ffmpeg_encoder)
+        encoding_thread.start()
+        
+        if keyboard.read_key() == "esc":
+            print("\nStopping Recording...")
+            self.running = False
+            
+            encoding_thread.join()
+            print('\nVideo saved.')
 
 if __name__ == '__main__':
-    main()
+    encoder = Encoder()
+    selected_encoder = encoder.set_encoder()
+    
+    recorder = ScreenRecorder(selected_encoder)
+    recorder.start_recording()
--- screen_capture.py
+++ screen_capture.py
@@ -1,43 +1,46 @@
-from win32api import GetSystemMetrics
 from mss import mss, exception
 import numpy as np
 import threading
+import pyautogui
 import keyboard
 import time
 import sys
 
-WIDTH, HEIGHT = GetSystemMetrics(0), GetSystemMetrics(1) # Gets monitor dimensions
-running = True
-FPS = 30
+class FrameCapture:
+    def __init__(self, fps=30):
+        self.width, self.height = pyautogui.size()
+        self.running = False
+        self.fps = fps
 
-def screen_capture():
-    try:
-        with mss() as sct:
-            monitor = {"top": 0, "left": 0, "width": WIDTH, "height": HEIGHT}
-            
-            while running:
-                last_time = time.time()
+    def screen_capture(self):
+        try:
+            with mss() as sct:
+                monitor = {"top": 0, "left": 0, "width": self.width, "height": self.height}
                 
-                frame = np.array(sct.grab(monitor))
-                sys.stdout.buffer.write(frame.tobytes())
+                while self.running:
+                    last_time = time.time()
+                    
+                    frame = np.array(sct.grab(monitor))
+                    sys.stdout.buffer.write(frame.tobytes())
+                    
+                    time_diff = time.time() - last_time
+                    time.sleep(max(1.0 / self.fps - time_diff, 0))
+                    
+                sys.stdout.buffer.close()
                 
-                time_diff = time.time() - last_time
-                time.sleep(max(1.0 / FPS - time_diff, 0))
-                
-            sys.stdout.buffer.close()
-            
-    except exception.ScreenShotError as e:
-        print("An error occurred while acquiring screen capture: ", e)
+        except exception.ScreenShotError as e:
+            print("An error occurred while acquiring screen capture: ", e)
 
-def main():
-    global running
-    
-    capture_thread = threading.Thread(target=screen_capture)
-    capture_thread.start()
-    
-    if keyboard.read_key() == "esc":
-        running = False
-        capture_thread.join()
+    def start_stream(self):
+        self.running = True
+        
+        capture_thread = threading.Thread(target=self.screen_capture)
+        capture_thread.start()
+        
+        if keyboard.read_key() == "esc":
+            self.running = False
+            capture_thread.join()
 
 if __name__ == '__main__':
-    main()
+    mss_stream = FrameCapture()
+    mss_stream.start_stream()
